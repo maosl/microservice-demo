5f98616466e109226dc9407bc059aec71adf752f
maosl <344985139@qq.com>
2020-12-11 10:23:00 +0800

    init

---
+++ util/ConsulEntitycs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace util
+{
+    public class ConsulEntity
+    {
+        public string Ip { get; set; }
+
+        public int Port { get; set; }
+
+        public string ServiceName { get; set; }
+
+        public string ConsulIp { get; set; }
+
+        public int ConsulPort { get; set; }
+    }
+}
